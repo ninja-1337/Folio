53d06cc09c1682d7fbfc4d75fee9d54f6da60fc6
unknown <you@example.com>
2023-04-26 19:07:28 +0300

    Add dark mode based on system theme

--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -17,11 +17,11 @@ const TechnologyCard = ({
         <Link href={documentation}>
         <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
 
-                <div>
+            
                 <h2 className="text-lg text-white-700">{name}</h2>
 
                 <p className="text-sm text-gray-400">{description}</p>
-                </div>
+           
 
         </section>
         </Link>
